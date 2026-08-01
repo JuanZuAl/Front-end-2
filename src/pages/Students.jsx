755a65766ef8b8394199f7c1c2d0f0e15a947b59
Juan Alberto Zuluaga Ramirez <Beto14zuluaga@gmail.com>
2026-08-01 17:27:37 -0500

    first commit

---
+++ src/pages/Students.jsx
@@ -0,0 +1,9 @@
+function students() {
+    return (
+        <div>
+            <h1>Students</h1>
+        </div>
+    );
+}
+
+export default students;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Courses from "../pages/Courses";
+import Students from "../pages/Students";
+import Dashboard from "../pages/Dashboard";
+import Enrollment from "../pages/Enrollment";
+
+function AppRoutes() {
+  return (
+    <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/students" element={<Students />} />
+        <Route path="/enrollment" element={<Enrollment />} />
+    </Routes>
+  );
+}
+
+export default AppRoutes;
